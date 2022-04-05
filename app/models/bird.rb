40082deafad1e1d8c087a61008e25b9df2409292
Stefan Inman <96351972+Anon-67@users.noreply.github.com>
2022-04-05 09:42:12 -0500

    commit tree 3e398afa21d186ded991b1f5b707dbd6beefeead

---
+++ app/models/bird.rb
@@ -0,0 +1,15 @@
+class Bird < ApplicationRecord
+
+    def index
+        render json: Bird.all
+    end
+
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: {error: "aint no fuckin bird"}, status: :not_found
+        end
+    end
+end
